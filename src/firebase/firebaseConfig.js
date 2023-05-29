51cd14f1b696e74978c83e2b4d56381a47f37908
badr ghanbi <bghanbi50@gmail.com>
2023-05-29 13:23:04 +0100

    add firebase and create the firebase config file

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAp2GKMbqhYMDqNIMiWxDbVUCuZCap7rLE",
+  authDomain: "multimartecommerce.firebaseapp.com",
+  projectId: "multimartecommerce",
+  storageBucket: "multimartecommerce.appspot.com",
+  messagingSenderId: "685421002116",
+  appId: "1:685421002116:web:b4a3b09ad6fd3cd363b1d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
