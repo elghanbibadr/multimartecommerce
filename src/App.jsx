4ad2b7a715afe24580744969989bdacd0691e3c8
badr ghanbi <bghanbi50@gmail.com>
2023-05-29 16:56:32 +0100

    create the base core for the testing app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import products from './data/products'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <div className='font-bold flex items-center justify-between bg-black text-white p-6'>
+      <h1>Multimart</h1>
+       <div className='flex cursor-pointer'>
+        <p className='mx-6'>login</p>
+        <p>sign up</p>
+       </div>
+    </div>
+    <div className='grid p-10 grid-cols-4 gap-12'>
+      {products.map(({id,price,productName,imgUrl}) => {
+        return <div key={id} className='my-10'>
+          <h1 className='text-xl font-bold'>{productName}</h1>
+          <img className='h-[70%] w-[70%]' src={imgUrl} alt="product image" />
+          <p>{price}$</p>
+        </div>
+      })}
+    </div> 
+    </>
   )
 }
 
