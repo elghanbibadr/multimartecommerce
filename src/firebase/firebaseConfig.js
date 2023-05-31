cc72e6bae463180a5d175cf90dbb44746be2894a
badr ghanbi <bghanbi50@gmail.com>
2023-05-31 17:22:20 +0100

    learned how to athenticate user with firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth  } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
  const auth = getAuth(app);
+ const db=getFirestore(app)
 const storage = getStorage(app);
-export {auth,storage}
+export {auth,storage,db}
 
