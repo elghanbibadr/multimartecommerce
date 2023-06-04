52a8ab2c6127e4b3d3d0141b7b7ddb53734dd1df
badr ghanbi <bghanbi50@gmail.com>
2023-06-04 16:06:50 +0100

    create the navbar version for mobiles

--- src/componenet/Navbar.jsx
+++ src/componenet/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bagShoppingLogo from "../assets/bag.png"
 import heartIcon from "../assets/heart-solid.svg"
 import cartIcon from "../assets/bag-shopping-solid.svg"
 import userIcon from "../assets/user-icon.png"
 import hamburgermenu from "../assets/hamburgermenu.svg"
 const Navbar = () => {
+    const [menuCollapse,setMenuCollapse] =useState(false)
+    const handleMenuCollapse=() => {
+     setMenuCollapse(prv => !prv)
+    }
   return (
-    <header className='p-2'>
-        <nav className='flex justify-between items-center'>
+    <header className='p-3'>
+        <nav className='flex justify-between flex-wrap  items-center'>
         <div className='flex'>
         <img className='h-5 w-5' src={bagShoppingLogo} alt='bag shopping' />
         <span className='font-bold text-primarycolor mx-2'>Multimart</span>
@@ -15,9 +19,14 @@ const Navbar = () => {
         <div className='flex items-center justify-between w-1/3'>
          <img className='h-5' src={heartIcon} alt="heart icon" />
          <img className='h-5 ' src={cartIcon} alt="cart icon" />
-         <img className='h-5' src={userIcon} alt="user icon" />
-         <img className='h-5' src={hamburgermenu} alt="user icon" />
+         <img className='h-6' src={userIcon} alt="user icon" />
+         <img onClick={handleMenuCollapse} className='h-5 ' src={hamburgermenu} alt="user icon" />
         </div>
+         { menuCollapse &&  <ul className='w-full text-primarycolor  '>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>Cart</li>
+        </ul>}
         </nav>
     </header>
   )
