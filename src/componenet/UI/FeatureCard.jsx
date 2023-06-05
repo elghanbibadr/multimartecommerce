4986343dc82c47fc41e22e68899335007800b2ed
badr ghanbi <bghanbi50@gmail.com>
2023-06-05 20:58:29 +0100

    create the fzture card componenet

--- src/componenet/UI/FeatureCard.jsx
+++ src/componenet/UI/FeatureCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import trackIcon from "../../assets/tracking.png"
 const FeatureCard = () => {
   return (
-    <div className='flex items-center bg-cardbg01 p-4 '>
-        <div className='bg-primarycolor rounded-full p-2'><img className='h-8' src={trackIcon} alt="track icon" /></div>
+    <div className='flex items-center w-[80%] mx-auto rounded-md mt-3 bg-cardbg01 p-3 '>
+        <div className='bg-primarycolor rounded-full p-1 mr-3'><img className='h-7' src={trackIcon} alt="track icon" /></div>
         <div>
-            <h4 className='text-primarycolor text-[1.1rem] font-semibold'>Free Shipping</h4>
-            <p>Lorem ipsum dolor sit amet</p>
+            <h4 className='text-primarycolor text-[1.1rem] font-medium '>Free Shipping</h4>
+            <p className='text-smalltextcolor'>Lorem ipsum dolor sit amet</p>
         </div>
     </div>
   )
