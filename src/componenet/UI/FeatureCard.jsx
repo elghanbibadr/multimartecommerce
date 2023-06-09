018af409720f2145931e0d162fa59d6fc06f321d
badr ghanbi <bghanbi50@gmail.com>
2023-06-09 11:53:47 +0100

    wrrap the feature section with the container componenet

--- src/componenet/UI/FeatureCard.jsx
+++ src/componenet/UI/FeatureCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import trackIcon from "../../assets/tracking.png"
+import Container from './Container'
 const FeatureCard = () => {
   return (
-    <div className='flex items-center w-[80%] mx-auto rounded-md mt-3 bg-cardbg01 p-3 '>
+    <Container>
+    <div className='flex items-center  mx-auto rounded-lg mt-3 bg-cardbg01 p-3 '>
         <div className='bg-primarycolor rounded-full p-1 mr-3'><img className='h-7' src={trackIcon} alt="track icon" /></div>
         <div>
             <h4 className='text-primarycolor text-[1.1rem] font-medium '>Free Shipping</h4>
             <p className='text-smalltextcolor'>Lorem ipsum dolor sit amet</p>
         </div>
     </div>
+    </Container>
   )
 }
 
