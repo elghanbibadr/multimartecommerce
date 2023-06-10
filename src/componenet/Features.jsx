d6c68bf53a325d91e2c6e3693d60b6e5c44728cc
badr ghanbi <bghanbi50@gmail.com>
2023-06-10 16:46:33 +0100

    add the key prop to the feature card componenett

--- src/componenet/Features.jsx
+++ src/componenet/Features.jsx
@@ -5,8 +5,8 @@ import Container from './UI/Container'
 const Features = () => {
   return (
     <Container className="md:grid md:grid-cols-2 lg:grid-cols-4">
-      {serviceData.map(({ title, subtitle, bg, icon }) => {
-        return <FeatureCard title={title} bg={bg} subtitle={subtitle} />
+      {serviceData.map(({ title, subtitle, bg, icon },index) => {
+        return <FeatureCard key={index} title={title} bg={bg} subtitle={subtitle} />
       })}
     </Container>
   )
--- src/componenet/UI/FeatureCard.jsx
+++ src/componenet/UI/FeatureCard.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import trackIcon from "../../assets/tracking.png"
 
 const FeatureCard = ({title,subtitle,bg}) => {
-  return (
+
+    return (
   
       <div  className={`flex items-center  mx-auto rounded-lg mt-3 h-fit p-3 `} style={{ backgroundColor: bg }}>
           <div className='bg-primarycolor rounded-full p-1 mr-3'><img className='h-7' src={trackIcon} alt="track icon" /></div>
