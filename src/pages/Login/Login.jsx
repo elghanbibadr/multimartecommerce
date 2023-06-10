c228111701f117766b2a58c4f359018f8b9ed10e
badr ghanbi <bghanbi50@gmail.com>
2023-06-10 16:36:57 +0100

    replace the user nmae input with the password input

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,17 @@
 import React ,{useState} from 'react'
+import { auth } from '../../../firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
 
+ const navigate=useNavigate()
 
 
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
   const handleEmailChange = (event) => {
@@ -17,23 +20,38 @@ const Login = () => {
 
 
 
-    const handleSubmit=() => {}
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      // User login successful
+      console.log('User logged in');
+      // Redirect to dashboard or desired route
+    } catch (error) {
+      // Handle login error
+      console.error('Error logging in:', error.message);
+    }finally{
+        navigate("/")
+    }
+  };
+
+
     return (
         <div className='mx-auto mt-20 w-[80%]'>
             <h2 className='text-3xl text-center mb-6 font-semibold text-primarycolor ' >Login</h2>
-            <form className='bg-primarycolor flex flex-col rounded-md items-center  text-white p-10' onSubmit={handleSubmit}>
-                <input
-
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={handleUsernameChange}
-                />
+            <form className='bg-primarycolor flex flex-col rounded-md items-center  text-white p-10' onSubmit={handleLogin}>
                 <input
                     type="email"
                     placeholder="Email"
                     value={email}
                     onChange={handleEmailChange}
+                />
+                <input
+
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={handlePasswordChange}
                 />
                
                 <button className='bg-white py-2 px-4 mb-6 font-medium rounded-md text-primarycolor' type="submit">Login</button>
