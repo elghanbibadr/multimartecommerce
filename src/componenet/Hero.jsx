11c3af9c570b01824fde624e8cf951ead2b6aae1
badr ghanbi <bghanbi50@gmail.com>
2023-06-11 12:33:55 +0100

    login user successfuly

--- src/componenet/Hero.jsx
+++ src/componenet/Hero.jsx
@@ -9,7 +9,8 @@ const Hero = () => {
         <div className='max-w-[1500px] mx-auto md:grid md:grid-cols-2 md:gap-x-16'>
             <div className=' self-center'>
                 <p className='text-lg md:text-xl' >Trending product in 2023</p>
-                { user &&  <p className='text-primarycolor'>hello {user.username}</p>}
+                {console.log(user)}
+                { user &&  <p className='text-primarycolor'>hello {user.email}</p>}
                 <h1 className='text-3xl my-4 font-medium'>Make Your Interior More Minimalistic & Modern</h1>
                 <p className=''>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat nulla repellat quo eaque alias corporis sunt, facilis nesciunt rem fugit!        </p>
