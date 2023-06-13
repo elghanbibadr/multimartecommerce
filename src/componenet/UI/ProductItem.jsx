1c340a0ed1684449db135008034c6f4f892b0fd1
badr ghanbi <bghanbi50@gmail.com>
2023-06-13 11:31:20 +0100

    fetch the cart data from firebase for each user

--- src/componenet/UI/ProductItem.jsx
+++ src/componenet/UI/ProductItem.jsx
@@ -1,72 +1,59 @@
 import React, { useContext, useId } from 'react'
 import item from "../../data/images/arm-chair-01.jpg"
 import { db } from '../../../firebaseConfig'
-import { doc,updateDoc ,getDoc,setDoc} from 'firebase/firestore'
+import { doc, updateDoc, getDoc, setDoc } from 'firebase/firestore'
 import { AppContext } from '../../Store/AppContext'
 
-const ProductItem = ({category,productName,imgUrl,price}) => {
-  const {user}=useContext(AppContext)
+const ProductItem = ({ category, productName, imgUrl, price }) => {
+  const { user } = useContext(AppContext)
 
-  const handleProductAddedToCart=async(userId)=>{
+  const handleProductAddedToCart = async (userId) => {
     console.log(user)
     // if the user not logged in return
     if (!user) {
       alert('please log in to add product to cart')
-    return
+      return
     }
     const userRef = doc(db, 'users', userId);
 
     // Get the user document from Firestore
     const userSnap = await getDoc(userRef);
     const userData = userSnap.data();
-  
-    // Check if the cart property exists, initialize it if it doesn't
-    // const cart = userData.cart || [];
-    // console.log(userData)
-  
-    // Add the item to the cart array with additional properties
-    const addedProduct={productName:productName,price:price,category:category,imgUrl:imgUrl} 
-  
-    // Update the user document with the new cart array
-    // if (userData.cart){
-    //   await setDoc(userRef, { email: userData.email, profilePhotoUrl: userData.profilePhotoUrl, uid: userData.uid, cart: [...userData.cart,addedProduct]});
-    // }else {
-    //   await setDoc(userRef, { email: userData.email, profilePhotoUrl: userData.profilePhotoUrl, uid: userData.uid, cart: [addedProduct ]});
 
-    // }
+
+    const addedProduct = { productName: productName, price: price, category: category, imgUrl: imgUrl }
+
+    // Update the user document with the new cart array
     const cart = userData.cart ? [...userData.cart, addedProduct] : [addedProduct];
 
-await setDoc(userRef, {
-  email: userData.email,
-  profilePhotoUrl: userData.profilePhotoUrl,
-  uid: userData.uid,
-  cart: cart
-});
+    await setDoc(userRef, {
+      email: userData.email,
+      profilePhotoUrl: userData.profilePhotoUrl,
+      uid: userData.uid,
+      cart: cart
+    });
+  }
 
-    // await updateDoc(userRef, { cart: updatedCart });
-  
-    console.log('Item added to cart successfully!');
-    }
   
   return (
     <>
-    
-    {productName && <div className='max-w-[340px] '>
+
+      {productName && <div className='max-w-[340px] '>
         <img className=' w-full mb-6' src={imgUrl} alt="product image" />
         <h3 className='text-primarycolor text-[1.2rem] font-semibold'>{productName}</h3>
         <p className='text-smalltextcolor my-[8px] font-medium'>{category}</p>
         <div className='flex justify-between items-center '>
-            <h6 className='text-[1.2rem] text-primarycolor font-semibold'>${price}</h6>
-            <div onClick={() => handleProductAddedToCart(user.uid)} className='bg-primarycolor cursor-pointer  h-[30px] mr-10 w-[30px] text-white  text-lg text-center rounded-full'>
-                +
-            </div>
+          <h6 className='text-[1.2rem] text-primarycolor font-semibold'>${price}</h6>
+          <div onClick={() => handleProductAddedToCart(user.uid)} className='bg-primarycolor cursor-pointer  h-[30px] mr-10 w-[30px] text-white  text-lg text-center rounded-full'>
+            +
+          </div>
         </div>
-    </div>
-    
-  }
+      </div>
+
+      }
     </>
   )
-  
+
 }
 
 export default ProductItem
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { db } from '../../../firebaseConfig';
+import { getDoc, doc } from 'firebase/firestore';
+import { AppContext } from '../../Store/AppContext';
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { user } = useContext(AppContext);
 
-export default Cart
+  useEffect(() => {
+    const getData = async () => {
+      if (user && user.uid) {
+        const userRef = doc(db, 'users', user.uid);
+
+        try {
+          const userSnap = await getDoc(userRef);
+          const userData = userSnap.data();
+          console.log(userData);
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      }
+    };
+
+    getData();
+  }, [user]);
+
+  return <div>Cart</div>;
+};
+
+export default Cart;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useContext, useEffect } from 'react'
+// import { db } from '../../../firebaseConfig'
+// import { getDoc,doc } from 'firebase/firestore';
+// import { AppContext } from '../../Store/AppContext';
+
+// const Cart = () => {
+//   const {user}=useContext(AppContext)
+//   const userRef = doc(db, 'users', user.uid);
+
+
+//   const getdata=async () =>{   
+//     if (!user)return 
+//       // Get the user document from Firestore
+//       const userSnap = await getDoc(userRef);
+//       const userData = userSnap.data();
+//       console.log(userData)
+      
+    
+//   }
+//   useEffect( () =>{
+//     getdata()
+//   },[])
+
+//   return (
+//     <div>Cart</div>
+//   )
+// }
+
+// export default Cart
