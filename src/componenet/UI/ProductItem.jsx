9449fd72e7dab8da6ff02d6802469c4dff046289
badr ghanbi <bghanbi50@gmail.com>
2023-06-13 10:44:06 +0100

    added item to cart successfully

--- src/componenet/UI/ProductItem.jsx
+++ src/componenet/UI/ProductItem.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useId } from 'react'
 import item from "../../data/images/arm-chair-01.jpg"
 import { db } from '../../../firebaseConfig'
-import { doc,updateDoc ,getDoc} from 'firebase/firestore'
+import { doc,updateDoc ,getDoc,setDoc} from 'firebase/firestore'
 import { AppContext } from '../../Store/AppContext'
 
 const ProductItem = ({category,productName,imgUrl,price}) => {
@@ -20,11 +20,22 @@ const ProductItem = ({category,productName,imgUrl,price}) => {
     const userSnap = await getDoc(userRef);
     const userData = userSnap.data();
   
-    // Add the item to the cart array
-    // const updatedCart = [...userData.cart, item];
+    // Check if the cart property exists, initialize it if it doesn't
+    // const cart = userData.cart || [];
+    // console.log(userData)
+  
+    // Add the item to the cart array with additional properties
+  
   
     // Update the user document with the new cart array
-    await updateDoc(userRef, { cart: [] });
+    if (userData.cart){
+      await setDoc(userRef, { email: userData.email, profilePhotoUrl: userData.profilePhotoUrl, uid: userData.uid, cart: [...userData.cart, productName]});
+    }else {
+      await setDoc(userRef, { email: userData.email, profilePhotoUrl: userData.profilePhotoUrl, uid: userData.uid, cart: [productName]});
+
+    }
+
+    // await updateDoc(userRef, { cart: updatedCart });
   
     console.log('Item added to cart successfully!');
     }
@@ -38,7 +49,7 @@ const ProductItem = ({category,productName,imgUrl,price}) => {
         <p className='text-smalltextcolor my-[8px] font-medium'>{category}</p>
         <div className='flex justify-between items-center '>
             <h6 className='text-[1.2rem] text-primarycolor font-semibold'>${price}</h6>
-            <div onClick={ handleProductAddedToCart} className='bg-primarycolor cursor-pointer  h-[30px] mr-10 w-[30px] text-white  text-lg text-center rounded-full'>
+            <div onClick={() => handleProductAddedToCart(user.uid)} className='bg-primarycolor cursor-pointer  h-[30px] mr-10 w-[30px] text-white  text-lg text-center rounded-full'>
                 +
             </div>
         </div>
