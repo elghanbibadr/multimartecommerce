2e7ab03d902db0bb85d7f362f24366b2de22915c
badr ghanbi <bghanbi50@gmail.com>
2023-06-16 15:13:42 +0100

    add the header for the cart componenet

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -7,7 +7,10 @@ const Cart = () => {
 
 
   console.log(itemsOnTheCart)
-  return <div className='bg-red-900 h-[100px]'>
+  return <div className=' '>
+    <div className='bg-primarycolor p-3'>
+      <h1 className='text-white font-bold text-center my-6'>Shopping Cart</h1>
+    </div>
     { !user &&   <h3 className='text-primarycolor text-lg'>please login to add item to cart</h3>}
     { user &&   itemsOnTheCart.length === 0  && <h3 className='text-primarycolor text-lg'>no item added to cart</h3>}
     { user &&    itemsOnTheCart.map(({productName,price,imgUrl},index)=>{
