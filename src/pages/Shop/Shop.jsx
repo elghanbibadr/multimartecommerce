920bc3a7f468ccfcc51316a804b8fde1f425249e
badr ghanbi <bghanbi50@gmail.com>
2023-06-19 13:49:16 +0100

    create the desktop version of the shop route body content

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -21,29 +21,31 @@ const Shop = () => {
     </div>
     <Container>
      {/* dropdown 1 */}
-       <div className='dropdown'>
-      <div className='dropdown-header' onClick={toggleDropdown}>
-        {selectedItem ? items.find(item => item.id == selectedItem).label : "Filter By Category"}
-        <img src={chevronIcon}  className={` h-3 icon ${isOpen && "open"}`}/>
-      </div>
-      <div className={`dropdown-body ${isOpen && 'open'}`}>
-        {items.map(item => (
-          <div className="dropdown-item" onClick={e => handleItemClick(e.target.id)} id={item.id}>
-            <span className={`dropdown-item-dot ${item.id == selectedItem && 'selected'}`}>• </span>
-            {item.label}
-          </div>
-        ))}
-      </div>
-    </div>
-    {/* search input */}
-    <form className='-z-20' >
-      <div className='flex justify-between p-2 rounded-md mt-4 items-center border-[1px]'>
-        <input className='outline-none p-0 bg-transparent m-0 border-none' type="text" placeholder='Search' />
-        <svg className='h-4  ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-         <path fill="#999" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
-        </svg>
-      </div>
-    </form>
+       <div className='md:flex md:justify-between '>
+         <div className='dropdown md:w-[300px]'>
+               <div className='dropdown-header' onClick={toggleDropdown}>
+          {selectedItem ? items.find(item => item.id == selectedItem).label : "Filter By Category"}
+          <img src={chevronIcon}  className={` h-3 icon ${isOpen && "open"}`}/>
+               </div>
+               <div className={`dropdown-body ${isOpen && 'open'}`}>
+          {items.map(item => (
+            <div className="dropdown-item" onClick={e => handleItemClick(e.target.id)} id={item.id}>
+              <span className={`dropdown-item-dot ${item.id == selectedItem && 'selected'}`}>• </span>
+              {item.label}
+            </div>
+          ))}
+               </div>
+             </div>
+             {/* search input */}
+             <form  className='md:w-1/2'  >
+               <div className='flex justify-between p-2 rounded-md mt-4 items-center border-[1px]'>
+          <input className='outline-none p-0 bg-transparent m-0 border-none' type="text" placeholder='Search' />
+          <svg className='h-4  ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+           <path fill="#999" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+          </svg>
+               </div>
+             </form>
+       </div>
       
     </Container>
     </>
