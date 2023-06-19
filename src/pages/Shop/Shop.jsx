ea66944a3fcd332824e80c42ee22f5767748dfb0
badr ghanbi <bghanbi50@gmail.com>
2023-06-19 12:34:18 +0100

    create the sort by select dropdown

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -10,14 +10,21 @@ const Shop = () => {
     </div>
     <Container>
       <form>
-        <select>
+        {/* filter by categor select dropdown */}
+        <select className='capitalize'>
           <option value="">filter by category</option>
           <option value="">sofa</option>
           <option value="">mobile</option>
           <option value="">chair</option>
           <option value="">watch</option>
           <option value="">wireless</option>
-         
+        </select>
+        {/* filter by categor select dropdown */}
+        <select>
+          <option value="">Sort by</option>
+          <option value="">Ascending</option>
+          <option value="">Descending</option>
+        
         </select>
       </form>
     </Container>
