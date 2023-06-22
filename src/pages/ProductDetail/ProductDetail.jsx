1128d6aa640b53abc06b66e4be1beba6c5d9fabf
badr ghanbi <bghanbi50@gmail.com>
2023-06-22 11:16:44 +0100

    add the key props to the rendered list of item

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -69,6 +69,7 @@ const ProductDetail = () => {
           </div>
 
         </div>
+            {/* reviews and desc */}
         <div>
           <div className='mt-8 flex font-medium  '>
             <h4 onClick={handleDescriptionClicked} className={`${isDescriptionActive ? "text-primarycolor" : "text-[#111]"} cursor-pointer`}>Description</h4>
@@ -76,8 +77,8 @@ const ProductDetail = () => {
           </div>
           {isDescriptionActive && <p className='mt-6 text-smalltextcolor text-base' > {currentProduct.item.description} </p>}
           {!isDescriptionActive && <div>
-            {currentProduct.item.reviews.map(({ text, rating }) => {
-              return <div className='mt-6'>
+            {currentProduct.item.reviews.map(({ text, rating },index) => {
+              return <div key={index} className='mt-6'>
                 <h4 className='text-[1.2rem] text-[#111]'>John Doe</h4>
                 <p className='text-orange-400'> {rating} (rating) </p>
                 <p className='text-smalltextcolor  mt-2'>{text}</p>
@@ -91,7 +92,6 @@ const ProductDetail = () => {
           }
          
         </div>
-        {/* reviews and desc */}
       </Container>}
     </>
   )
